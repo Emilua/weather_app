ed31ae461c312cbb864057f1474db1f7ee873c74
Emilua <37698286+Emilua@users.noreply.github.com>
2022-10-27 19:22:12 +0300

    Create all project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from "react";
+import axios from "axios";
 
-function App() {
+export default function Search() {
+  let [city, setCity] = useState(" ");
+  let [temp, setTemp] = useState(" ");
+  let [description, setDescription] = useState(" ");
+  let [humidity, setHumidity] = useState(" ");
+  let [wind, setWind] = useState(" ");
+  let [icon, setIcon] = useState(" ");
+  let [ul, setUl] = useState(" ");
+
+  let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=6605fc03f7aea0369923c76b4eb46d07&units=metric`;
+
+  function showWeather(response) {
+    setTemp(Math.round(response.data.main.temp));
+    setDescription(response.data.weather[0].description);
+    setHumidity(response.data.main.humidity);
+    setWind(response.data.wind.speed);
+    setIcon(
+      `http://openweathermap.org/img/w/${response.data.weather[0].icon}.png`
+    );
+  }
+  function handelSubmit(event) {
+    event.preventDefault();
+    axios.get(url).then(showWeather);
+  }
+
+  function updateCity(event) {
+    setCity(event.target.value);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Weather App</h1>
+      <form onSubmit={handelSubmit}>
+        <input type="search" onChange={updateCity} />
+        <input type="submit" value="Search" />
+        <ul>
+          <li>Temperature: {temp}°C</li>
+          <li>Description: {description}</li>
+          <li>Humidity: {humidity}</li>
+          <li>Wind: {wind}</li>
+          <li>
+            <img src={icon} alt="icon" />
+          </li>
+        </ul>
+      </form>
+      <a href=''>My project</a>developed Emil
     </div>
   );
 }
-
-export default App;
